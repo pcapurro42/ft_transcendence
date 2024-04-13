1934cfe88d4251f7d7f2bb37b8be6152ad39d4ae
Benjamin Gales <bjmgales@gmail.com>
2024-04-13 14:00:30 +0200

    add: goat sound effect

---
+++ sound-toggle.js
@@ -0,0 +1,14 @@
+document.getElementById('soundtoggle').addEventListener('click', function(){
+	let audio = document.getElementById('mgs');
+	let img = document.getElementById('soundtoggle')
+
+	if (audio.muted){
+		audio.muted = false;
+		audio.play();
+		img.src = './ressources/images/sound-on.png';
+	}
+	else{
+		audio.muted = true;
+		img.src = './ressources/images/sound-off.png';
+	}
+})
