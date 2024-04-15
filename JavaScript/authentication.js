366eb11db00456e32013ff2543909e7fb6aaf49a
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-15 22:13:03 +0200

    added lines in auth() function in authentication.js

--- JavaScript/authentication.js
+++ JavaScript/authentication.js
@@ -1,5 +1,7 @@
 let login_id = document.getElementById("login-42");
 
+localStorage.setItem("status", "not connected");
+
 if (localStorage.getItem("status") == "connected")
     login_id.innerHTML = "CONNECTED";
 else
@@ -9,5 +11,9 @@ login_id.onclick = auth;
 
 function auth()
 {
-    console.log("authentication...");
+    if (localStorage.getItem("status") == "not connected")
+    {
+        localStorage.setItem("status", "connected");
+        login_id.innerHTML = "CONNECTED";
+    }
 }
