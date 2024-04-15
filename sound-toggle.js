05023595a843d130123dddbb5bf96692ddcf9748
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-15 21:01:06 +0200

    added a background.css file to store the background loading, moved mgs1-alert sound to sound folder, renamed mgs2 theme file, reorganized sound-toggle.js to look like authentication.js

--- sound-toggle.js
+++ sound-toggle.js
@@ -1,14 +1,19 @@
-document.getElementById('soundtoggle').addEventListener('click', function(){
+document.getElementById('soundtoggle').onclick = audio;
+
+function audio()
+{
 	let audio = document.getElementById('mgs');
 	let img = document.getElementById('soundtoggle')
 
-	if (audio.muted){
+	if (audio.muted)
+	{
 		audio.muted = false;
 		audio.play();
 		img.src = './ressources/images/sound-on.png';
 	}
-	else{
+	else
+	{
 		audio.muted = true;
 		img.src = './ressources/images/sound-off.png';
 	}
-})
+}
