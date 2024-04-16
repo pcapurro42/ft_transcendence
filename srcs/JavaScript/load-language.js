40ba920bdefe19cb4eb7fbd9bc92fb25897bb061
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-16 21:19:41 +0200

    renamed languages files, wrote the start of load-languages

---
+++ srcs/JavaScript/load-language.js
@@ -0,0 +1,12 @@
+let lang = localStorage.getItem("language");
+let filename = "./Languages/" + lang + ".json";
+
+if (localStorage.getItem("language") == "en")
+{
+    ;
+}
+
+if (localStorage.getItem("language") == "fr")
+{
+    ;
+}
