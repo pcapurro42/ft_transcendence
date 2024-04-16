aa66574b897eb3c712c65150528b1fdf2d2071f8
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-16 20:38:31 +0200

    removed random number call for tab_icon, logo of fox by default, removing it later

--- srcs/JavaScript/initialization.js
+++ srcs/JavaScript/initialization.js
@@ -1,16 +1,5 @@
 if (localStorage.getItem("language") == null)
     localStorage.setItem("language", "en");
 
-{
-    let value = generateNumber(3);
-
-    if (value == 1)
-        tab_icon.innerHTML = "";
-    else if (value == 2)
-        tab_icon.innerHTML = "";
-    else if (value == 3)
-        tab_icon.innerHTML = "";
-}
-
 if (localStorage.getItem("status") == null)
     localStorage.setItem("status", "not connected");
