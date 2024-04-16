81dcf4ffacc9e79f8559ac18396f324749570eae
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-16 20:19:23 +0200

    added generateNumbe() function, wrote tab_icon initialization

--- srcs/JavaScript/initialization.js
+++ srcs/JavaScript/initialization.js
@@ -1,5 +1,16 @@
 if (localStorage.getItem("language") == null)
     localStorage.setItem("language", "en");
 
+{
+    let value = generateNumber(3);
+
+    if (value == 1)
+        tab_icon.innerHTML = "";
+    else if (value == 2)
+        tab_icon.innerHTML = "";
+    else if (value == 3)
+        tab_icon.innerHTML = "";
+}
+
 if (localStorage.getItem("status") == null)
     localStorage.setItem("status", "not connected");
