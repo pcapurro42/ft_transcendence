70fbd1df13b51e6fc002aca238eb170e756bb2cb
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-17 00:15:12 +0200

    added security for language change

--- srcs/JavaScript/load-language.js
+++ srcs/JavaScript/load-language.js
@@ -3,11 +3,17 @@ let lang = localStorage.getItem("language");
 if (lang == "en")
 {
     for (let i = 0; i != en.length; i++)
-        document.getElementById(en[i][0]).innerHTML = en[i][1];
+    {
+        if (getElementById(en[i][0]) != null)
+            document.getElementById(en[i][0]).innerHTML = en[i][1];
+    }
 }
 
 if (lang == "fr")
 {
     for (let i = 0; i != fr.length; i++)
-        document.getElementById(fr[i][0]).innerHTML = fr[i][1];
+    {
+        if (getElementById(fr[i][0]) != null)
+            document.getElementById(fr[i][0]).innerHTML = fr[i][1];
+    }
 }
