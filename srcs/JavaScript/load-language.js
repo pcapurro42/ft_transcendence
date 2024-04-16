9ea333dcefaf6aa83a7e251fcd289a9d5288d0aa
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-17 00:17:53 +0200

    fixed security problem for language change

--- srcs/JavaScript/load-language.js
+++ srcs/JavaScript/load-language.js
@@ -4,7 +4,7 @@ if (lang == "en")
 {
     for (let i = 0; i != en.length; i++)
     {
-        if (getElementById(en[i][0]) != null)
+        if (document.getElementById(en[i][0]) != null)
             document.getElementById(en[i][0]).innerHTML = en[i][1];
     }
 }
@@ -13,7 +13,7 @@ if (lang == "fr")
 {
     for (let i = 0; i != fr.length; i++)
     {
-        if (getElementById(fr[i][0]) != null)
+        if (document.getElementById(fr[i][0]) != null)
             document.getElementById(fr[i][0]).innerHTML = fr[i][1];
     }
 }
