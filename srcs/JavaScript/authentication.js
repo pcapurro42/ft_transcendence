26846a20c5ca9da162924b026e1dc3bebb65f4af
Benjamin Gales <bjmgales@gmail.com>
2024-04-16 14:55:08 +0200

    add: function generateCodeVerifier()

--- srcs/JavaScript/authentication.js
+++ srcs/JavaScript/authentication.js
@@ -23,6 +23,20 @@ function log()
     }
 }
 
+function generateCodeVerifier(){
+    let bin_tab = new Uint8Array(32) // = 44 caracteres
+    window.crypto.getRandomValues(bin_tab);
+
+    const to_chars = String.fromCharCode(...bin_tab);
+    const url_formatted = btoa(to_chars).replace(/\+/g, '-')
+    .replace(/\//g, '_').replace(/=+$/, '');
+    return url_formatted;
+}
+
+// async function generateCodeChallenger(){
+//     const encoder = new TextEncoder();
+//     console.log(encoder.encode(generateCodeVerifier));
+// }
 function connect()
 {
     ;
