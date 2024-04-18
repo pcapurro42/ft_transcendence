255901ce69bac93d0898edc3c6b1be1e94a932e5
Benjamin Gales <bjmgales@gmail.com>
2024-04-19 00:49:02 +0200

    add: goat sounds

--- srcs/JavaScript/sound-toggle.js
+++ srcs/JavaScript/sound-toggle.js
@@ -17,3 +17,40 @@ function audio()
 		img.src = './Materials/images/sound-off.png';
 	}
 }
+
+function hoverSound(event) {
+    let audio = document.getElementById('mgs');
+    let hoverSound = document.getElementById('hover_sound');
+
+    if (!audio.muted) {
+        hoverSound.play();
+    }
+
+    event.stopPropagation();
+}
+
+function clickSound(event) {
+    let audio = document.getElementById('mgs');
+    let clickSound = document.getElementById('click_sound');
+
+    if (!audio.muted) {
+        clickSound.play();
+    }
+
+    event.stopPropagation();
+}
+
+const buttons = document.getElementsByClassName('btn');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('mouseover', hoverSound);
+    buttons[i].addEventListener('click', clickSound);
+}
+
+const settingsModal = document.getElementById('settings_modal');
+const closeSound = document.getElementById('close_sound');
+
+settingsModal.addEventListener('hidden.bs.modal', function () {
+    let audio = document.getElementById('mgs');
+    if (!audio.muted)
+        closeSound.play();
+});
