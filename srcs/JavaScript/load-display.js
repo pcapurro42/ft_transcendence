49b11e125742639c642c77f2dcbeeb9f3345a867
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-21 13:48:15 +0200

    removed background.css, added functions for load-display.js, added class 'text'

--- srcs/JavaScript/load-display.js
+++ srcs/JavaScript/load-display.js
@@ -1,4 +1,31 @@
+refreshDisplay();
+
+function setBackground(color)
+{
+    let background = document.getElementById("background");
+    if (color == "white")
+        background.style.setProperty("background-color", "white");
+    else
+        background.style.setProperty("background-color", "black");
+}
+
+function setText(color)
+{
+    let texts = document.getElementsByClassName("text");
+    for (let i = 0; i != texts.length; i++)
+    {
+        if (color == "white")
+            texts[i].classList.add("text-white"), texts[i].classList.remove("text-black");
+        else
+            texts[i].classList.add("text-black"), texts[i].classList.remove("text-white");
+    }
+}
+
 function refreshDisplay()
 {
-    
+
+    if (high_contrast_colors == true)
+        setBackground("black"), setText("white");
+    else
+        setBackground("white"), setText("black");
 }
