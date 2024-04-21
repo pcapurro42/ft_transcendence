f6a6feca3e17040000851c6ff410f12bc8e7a36e
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-21 23:20:39 +0200

    added play.js file and displayPlay()/removePlay()

---
+++ host/front-end/JavaScript/play.js
@@ -0,0 +1,20 @@
+document.getElementById('play_btn').onclick = displayPlay;
+// document.getElementById('play_back_btn').onclick = removePlay;
+
+function displayPlay()
+{
+    let main_menu = document.getElementById('main_menu_buttons');
+    // let play_menu = document.getElementById('play_menu');
+
+    // play_menu.style.display = 'block';
+    main_menu.style.display = 'none';
+}
+
+function removePlay()
+{
+    let main_menu = document.getElementById('main_menu_buttons');
+    // let play_menu = document.getElementById('play_menu');
+
+    // play_menu.style.display = 'none';
+    main_menu.style.display = 'block';
+}
