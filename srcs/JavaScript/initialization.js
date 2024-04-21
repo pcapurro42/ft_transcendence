6d7914ad8cca258f37b8ce8c2f0d8b8631e237c9
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-21 13:32:58 +0200

    added global variables for settings, removed settings.js, added load-display.js

--- srcs/JavaScript/initialization.js
+++ srcs/JavaScript/initialization.js
@@ -1,22 +1,29 @@
 // < SETTINGS > //
 
+let language;
+let sounds_volume;
+let music_volume;
+let text_size;
+let descriptive_images;
+let high_contrast_colors;
+
 if (localStorage.getItem("language") == null)
-    localStorage.setItem("language", "en");
+    localStorage.setItem("language", "en"), language = "en";
 
 if (localStorage.getItem("sounds_volume") == null)
-    localStorage.setItem("sounds_volume", "100");
+    localStorage.setItem("sounds_volume", "100"), sounds_volume = "100";
 
 if (localStorage.getItem("music_volume") == null)
-    localStorage.setItem("music_volume", "100");
+    localStorage.setItem("music_volume", "100"), music_volume = "100";
 
 if (localStorage.getItem("text_size") == null)
-    localStorage.setItem("text_size", "normal");
+    localStorage.setItem("text_size", "normal"), text_size = "normal";
 
 if (localStorage.getItem("descriptive_images") == null)
-    localStorage.setItem("descriptive_images", "false");
+    localStorage.setItem("descriptive_images", "false"), descriptive_images = "false";
 
 if (localStorage.getItem("high_contrast_colors") == null)
-    localStorage.setItem("high_contrast_colors", "false");
+    localStorage.setItem("high_contrast_colors", "false"), high_contrast_colors = "false";
 
 // < 42 CONNECT > //
 
