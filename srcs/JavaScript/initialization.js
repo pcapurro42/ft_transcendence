ba18db5fea2d22460120516fc050be2d9d6cf8fc
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-21 13:24:23 +0200

    added default settings in initialization.js

--- srcs/JavaScript/initialization.js
+++ srcs/JavaScript/initialization.js
@@ -1,5 +1,24 @@
+// < SETTINGS > //
+
 if (localStorage.getItem("language") == null)
     localStorage.setItem("language", "en");
+
+if (localStorage.getItem("sounds_volume") == null)
+    localStorage.setItem("sounds_volume", "100");
+
+if (localStorage.getItem("music_volume") == null)
+    localStorage.setItem("music_volume", "100");
+
+if (localStorage.getItem("text_size") == null)
+    localStorage.setItem("text_size", "normal");
+
+if (localStorage.getItem("descriptive_images") == null)
+    localStorage.setItem("descriptive_images", "false");
+
+if (localStorage.getItem("high_contrast_colors") == null)
+    localStorage.setItem("high_contrast_colors", "false");
+
+// < 42 CONNECT > //
 
 if (localStorage.getItem("status") == null)
     localStorage.setItem("status", "not connected");
@@ -8,7 +27,3 @@ if (localStorage.getItem("status") == "not connected")
     document.getElementById('welcome').style.visibility = "hidden";
 else
     document.getElementById('welcome').style.display = "block";
-
-localStorage.setItem("language", "en");
-
-document.getElementById("main_menu_settings").style.display = "none";
