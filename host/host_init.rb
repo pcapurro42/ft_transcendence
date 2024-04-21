87c85f6433a8d721784c646cbe4461c661cc66e2
pcapurro42 <pcapurro@student.42nice.fr>
2024-04-21 19:50:15 +0200

    added more explainations in host_init.rb, binded 127.0.0.1, removed useless logs, added debug message

--- host/host_init.rb
+++ host/host_init.rb
@@ -3,11 +3,13 @@ require 'webrick'
 
 workdir = File.expand_path '.'
 # Attribution à workdir du chemin absolu de '.'
-server = WEBrick::HTTPServer.new :Port => 443, :DocumentRoot => workdir
+server = WEBrick::HTTPServer.new :Port => 443, :Bindaddress => '127.0.0.1', :Logger => WEBrick::Log.new(File::NULL, WEBrick::Log::FATAL), :DocumentRoot => workdir
 # Création du serveur sur le port 443 avec comme répertoire racine '.'
+# À l'exception des logs d'accès, les logs du serveur sont redirigés pour ne pas apparaître dans la console
+# 'WEBrick::Log::FATAL' définit les erreurs fatales comme niveau minimum d'erreur autorisé à être affiché
 
 trap 'INT' do
-    puts "Ctrl-C detected. Shutting down server..."
+    puts "\r^C detected. Shutting down server..."
     server.shutdown 
 end
 # Réception du signal INT (= SIGINT = Ctrl-C) pour éteindre le serveur
