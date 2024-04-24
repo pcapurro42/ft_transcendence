ad9a2d76b18bde316409dde065b43a0b2567a6bc
Benjamin Gales <bjmgales@gmail.com>
2024-04-24 15:36:27 +0200

    add: CORS handled 🎉

--- host/front-end/JavaScript/auth.js
+++ host/front-end/JavaScript/auth.js
@@ -4,7 +4,7 @@ window.onload = handleRedirection;
 /****************************************** Log in | Log Out ***********************************************/
 
 const client_id = 'u-s4t2ud-328d5957a0e78853f7b035bed31812c4bd82ea90773c43b8686b35f1ae4d1353';
-const redirect_uri = 'http://127.0.0.1:443';
+const redirect_uri = 'https://127.0.0.1';
 
 async function log()
 {
@@ -62,7 +62,7 @@ async function handleRedirection(){
 
 async function sendAccessToken(){
     console.log("sendAccessToken says hello")
-    const endpoint = 'https://corsproxy.io/?https://api.intra.42.fr/v2/me';
+    const endpoint = '/v2/me';
     const request = await fetch(endpoint, {
         method: 'GET',
         headers: {'Authorization' : `Bearer ${sessionStorage.getItem('accessToken')}`}
@@ -80,7 +80,7 @@ async function sendAccessToken(){
 
 async function getAccessToken(auth_code){
     console.log("getAccessToken says hello")
-    const endpoint = 'https://corsproxy.io/?https://api.intra.42.fr/oauth/token';
+    const endpoint = '/oauth/token';
 
     const url = new URLSearchParams({
         client_id: client_id,
@@ -104,30 +104,6 @@ async function getAccessToken(auth_code){
         console.error(`Error: ${err}`);
     }
 }
-
-
-// function generateCodeVerifier(){
-//     let bin_tab = new Uint8Array(32) // = 44 caracteres
-//     window.crypto.getRandomValues(bin_tab);
-
-//     const to_chars = String.fromCharCode(...bin_tab);
-//     const url_formatted = btoa(to_chars).replace(/\+/g, '-')
-//     .replace(/\//g, '_').replace(/=+$/, '');
-//     sessionStorage.setItem('code_verifier', url_formatted)
-// }
-
-// async function generateCodeChallenge(){
-//     const encoder = new TextEncoder(); // on instancie TextEncoder
-//     const bin_tab = encoder.encode(sessionStorage.getItem('code_verifier')); //on repasse le code_verifier en binaire pour l'encryption
-//     const hash = await crypto.subtle.digest('SHA-256', bin_tab);// on encrypte en SHA-256
-//     const code_challenge = btoa(String.fromCharCode(...new Uint8Array(hash)))
-//     .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''); //on met au format URL.
-//     return (code_challenge);
-// }
-
-/******************************************************** DISPLAY *************************************************************/
-
-
 
 if (localStorage.getItem("status") == "connected")
     login_btn.setAttribute("data-oname", "LOG OUT");
