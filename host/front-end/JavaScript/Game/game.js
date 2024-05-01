502773e817287c6659f7632c3d441c6fa7e900e2
pcapurro42 <pcapurro@student.42nice.fr>
2024-05-01 16:18:07 +0200

    added background color for game canvas

--- host/front-end/JavaScript/Game/game.js
+++ host/front-end/JavaScript/Game/game.js
@@ -1,5 +1,13 @@
+/** @type {HTMLCanvasElement} */
+
 const canvas = document.getElementById('game');
 let display = canvas.getContext('2d');
 
 canvas.width = 1100;
 canvas.height = 720;
+
+// let theme = "black";
+theme = "white";
+
+display.fillStyle = theme;
+display.fillRect(0, 0, canvas.width, canvas.height);
