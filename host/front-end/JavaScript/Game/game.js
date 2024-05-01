e034213c902e6c44f043061532f503efa69a53e7
pcapurro42 <pcapurro@student.42nice.fr>
2024-05-01 16:32:38 +0200

    added bar class to represent players on canvas

--- host/front-end/JavaScript/Game/game.js
+++ host/front-end/JavaScript/Game/game.js
@@ -11,3 +11,44 @@ theme = "white";
 
 display.fillStyle = theme;
 display.fillRect(0, 0, canvas.width, canvas.height);
+
+let bar_speed = 10;
+
+let bar_height = 135;
+let bar_width = 30;
+
+class Bar
+{
+    constructor(width, height, x, y, speed)
+    {
+        this.width = width;
+        this.height = height;
+
+        this.x = x;
+        this.y = y;
+
+        this.speed = speed;
+    }
+
+    moveUp()
+    {
+        ;
+    }
+
+    moveDown()
+    {
+        ;
+    }
+
+    display()
+    {
+        display.fillStyle = "green";
+        display.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+let left_player = new Bar(bar_width, bar_height, 0, 150, bar_speed);
+left_player.display();
+
+let right_player = new Bar(bar_width, bar_height, canvas.width - bar_width, 400, bar_speed);
+right_player.display();
