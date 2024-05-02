3edc8605dcb7034e17f00b1c08b561e6577b5810
pcapurro42 <pcapurro@student.42nice.fr>
2024-05-02 19:48:50 +0200

    removed Game folder in Game/, removed objects.js and merged it with game.js

--- host/front-end/JavaScript/Game/game.js
+++ host/front-end/JavaScript/Game/game.js
@@ -3,6 +3,89 @@
 theme = "dark";
 // theme = "light";
 
+// < OBJECT > //
+
+class Bar
+{
+    constructor(width, height, x, y, speed, color)
+    {
+        this.width = width;
+        this.height = height;
+
+        this.x = x;
+        this.y = y;
+
+        this.speed = speed;
+
+        this.color = color;
+    }
+
+    print()
+    {
+        infos.display.fillStyle = infos.bar_color;
+        infos.display.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    moveUp()
+    {
+        if (this.y > 0)
+        {
+            displayBackground();
+            this.y = this.y - this.speed;
+            this.print();
+        }
+    }
+
+    moveDown()
+    {
+        if (this.y + infos.bar_height < infos.game_height)
+        {
+            displayBackground();
+            this.y = this.y + this.speed;
+            this.print();
+        }
+    }
+
+    getInfo()
+    {
+        return ([this.x, this.y]);
+    }
+}
+
+// < INFO STRUCT > //
+
+infos = {
+    canvas: null,
+    display: null,
+
+    game_width: 1100,
+    game_height: 720,
+
+    bar_speed: 10,
+
+    bar_height: 100,
+    bar_width: 20,
+
+    separator_height: 20,
+    separator_width: 10,
+    separator_space: 17,
+
+    menu_color: null,
+    background_color: null,
+    bar_color: null
+}
+
+// < GAME STRUCT > //
+
+game = {
+    left_player: null,
+    right_player: null,
+
+    ball: null,
+
+    scores: [0, 0]
+}
+
 // < CONSTANTS > //
 
 const x_center = infos.game_width / 2;
@@ -108,8 +191,8 @@ function refreshGameDisplay(event)
     displayCenterBar();
     displayScores();
 
-    console.log(game.left_player.getInfo());
-    console.log(game.right_player.getInfo());
+    // console.log(game.left_player.getInfo());
+    // console.log(game.right_player.getInfo());
 }
 
 window.addEventListener('keydown', (event) => {
