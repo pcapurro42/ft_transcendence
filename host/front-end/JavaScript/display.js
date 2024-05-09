ffbbda8a3f8e6f24a929769c6dab708a26a93962
l1mpoln <kuzminvova342@gmail.com>
2024-05-09 19:37:55 +0200

    add: little fix of stats and history html

--- host/front-end/JavaScript/display.js
+++ host/front-end/JavaScript/display.js
@@ -30,6 +30,12 @@ function displayMenu()
     let settings_menu = document.getElementById('settings_menu');
     settings_menu.style.display = "none";
 
+    let stats_menu = document.getElementById('stats_menu');
+    stats_menu.style.display = "none";
+
+    let history = document.getElementById('history');
+    history.style.display = "none";
+
     let play_classic_buttons = document.getElementById('one_vs_one_online_choice_menu');
     play_classic_buttons.style.display = "none";
 
