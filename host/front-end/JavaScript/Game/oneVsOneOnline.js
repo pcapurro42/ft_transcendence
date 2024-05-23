f470450ce1e050636e891e17a49c85bb84c81048
pcapurro42 <pcapurro@student.42nice.fr>
2024-05-23 17:43:49 +0200

    removed tournament part from local 1v2 and online 1v1 since it's not necessary here

--- host/front-end/JavaScript/Game/oneVsOneOnline.js
+++ host/front-end/JavaScript/Game/oneVsOneOnline.js
@@ -1,16 +1,15 @@
-// <<<<<<< 2V2 LOCAL >>>>>>> //
+// <<<<<<< 1V1 >>>>>>> //
 
 // < Game class > //
 
-class LocalGame1v2
+class OnlineGame1v1
 {
     constructor()
     {
-        this.player_nb = 3;
+        this.player_nb = 2;
 
         this.left_player = null;
-        this.right_player_1 = null;
-        this.right_player_2 = null;
+        this.right_player = null;
 
         this.ball = null;
 
@@ -21,6 +20,7 @@ class LocalGame1v2
         this.scores = [0, 0];
 
         this.canvas = null;
+        this.display = null;
 
         this.game_width = 1100;
         this.game_height = 720;
@@ -43,6 +43,8 @@ class LocalGame1v2
         this.separator_width = 3;
         this.separator_space = 17;
 
+        this.sounds = null;
+
         this.alert = 0;
     }
 
@@ -50,7 +52,10 @@ class LocalGame1v2
     {
         // canvas creation
 
-        this.canvas = document.getElementById('one_vs_two_local_game');
+        if (type == "tournament")
+            this.canvas = document.getElementById('tournament_game');
+        else
+            this.canvas = document.getElementById('one_vs_one_local_game');
         this.display = this.canvas.getContext('2d');
 
         this.canvas.width = this.game_width;
@@ -86,39 +91,21 @@ class LocalGame1v2
             color: this.bar_color
         }
 
-        let right_player_one_data = {
+        let right_player_data = {
             game: this,
 
             object_width: this.bar_width,
             object_heigth : this.bar_height,
 
             map_x: ((this.game_width - this.bar_width) - this.bar_width),
-            map_y: ((this.game_height / 4) - this.bar_height / 2),
+            map_y: ((this.game_height / 2) - this.bar_height / 2),
 
             bar_speed: this.bar_speed,
-            color: this.bar_color,
-
-            pos: "up"
+            color: this.bar_color
         }
 
-        let right_player_two_data = {
-            game: this,
-
-            object_width: this.bar_width,
-            object_heigth : this.bar_height,
-
-            map_x: ((this.game_width - this.bar_width) - this.bar_width),
-            map_y: ((this.game_height / 2 + this.game_height / 4) - this.bar_height / 2),
-
-            bar_speed: this.bar_speed,
-            color: this.bar_color,
-
-            pos: "down"
-        }
-
-        this.left_player = new Bar1v1(...Object.values(left_player_data))
-        this.right_player_1 = new Bar1v2(...Object.values(right_player_one_data));
-        this.right_player_2 = new Bar1v2(...Object.values(right_player_two_data));
+        this.left_player = new Bar1v1(...Object.values(left_player_data));
+        this.right_player = new Bar1v1(...Object.values(right_player_data));
 
         // ball creation
 
@@ -126,58 +113,59 @@ class LocalGame1v2
 
         x = this.game_width / 2 - (this.ball_width / 2);
         y = this.game_height / 2 - (this.ball_width / 2);
-        
+
         let ball_data = {
             game: this,
-        
+
             object_width: this.ball_width,
             object_heigth: this.ball_height,
-        
+
             x_pos : x,
             y_pos : y,
-        
+
             speed: this.ball_speed,
             color: this.ball_color,
-        
+
             direction : this.ball_direction,
             bonus_speed: 0
         }
-        
+
         this.ball = new Ball(...Object.values(ball_data));
-        
+
         // bonus creation
-        
+
         if (gameMode != "normal")
         {
             let bonus_one_data = {
                 game: this,
-        
+
                 object_width: this.ball_width,
                 object_heigth: this.ball_height,
-        
+
                 x_pos : (this.game_width / 4),
                 y_pos : (this.game_height / 2),
-        
+
                 speed: 2,
                 color: this.bonus_color,
-        
+
                 direction : this.ball_direction,
             }
-        
+
             let bonus_two_data = {
                 game: this,
-        
+
                 object_width: this.ball_width,
                 object_heigth: this.ball_height,
-        
+
                 x_pos : (this.game_width / 2 + (this.game_width / 4)),
                 y_pos : (this.game_height / 2),
+
                 speed: 2,
                 color: this.bonus_color,
-        
+
                 direction : this.ball_direction + 90,
             }
-        
+
             this.bonus_one = new PowerUp(...Object.values(bonus_one_data));
             this.bonus_two = new PowerUp(...Object.values(bonus_two_data));
         }
@@ -200,7 +188,7 @@ class LocalGame1v2
         this.refreshScores();
         this.refreshPlayers();
         this.refreshBall();
-    
+
         if (gameMode != "normal")
             this.refreshBonus();
     }
@@ -210,25 +198,25 @@ class LocalGame1v2
         this.display.fillStyle = this.background_color;
         this.display.fillRect(0, 0, this.game_width, this.game_height);
     }
-    
+
     refreshCenterBar()
     {
         let x_bar_center = (this.game_width / 2) - (this.separator_width / 2);
         let nb = ~~(this.game_height / (this.separator_height + this.separator_space));
-    
+
         this.display.fillStyle = this.menu_color;
         for (let value = 0; value != nb; value++)
         {
             this.display.fillRect(x_bar_center, ((this.separator_height * value) + this.separator_space * (value + 1)), this.separator_width, this.separator_height);
         }
     }
-    
+
     refreshScores()
     {
         let score_y = this.game_height / 6;
         let left_score_x = (this.game_width / 4) - this.text_size / 4;
         let right_score_x = (this.game_width - this.game_width / 4) - this.text_size / 4;
-    
+
         this.display.font = this.text_size + "px " + this.text_font;
         this.display.fillText(this.scores[0], left_score_x, score_y);
         this.display.fillText(this.scores[1], right_score_x, score_y);
@@ -241,22 +229,16 @@ class LocalGame1v2
         if (keys.KeyD == true)
             this.left_player.moveDown();
 
-        if (keys.KeyU == true)
-            this.right_player_1.moveUp();
-        if (keys.KeyJ == true)
-            this.right_player_1.moveDown();
-
         if (keys.ArrowUp == true)
-            this.right_player_2.moveUp();
+            this.right_player.moveUp();
         if (keys.ArrowDown == true)
-            this.right_player_2.moveDown();
+            this.right_player.moveDown();
 
         this.left_player.print();
-        this.right_player_1.print();
-        this.right_player_2.print();
+        this.right_player.print();
 
         if (gameMode != "normal")
-            this.left_player.displayBonus(), this.right_player_1.displayBonus(), this.right_player_2.displayBonus();
+            this.left_player.displayBonus(), this.right_player.displayBonus();
     }
 
     refreshBall()
@@ -268,7 +250,7 @@ class LocalGame1v2
         this.ball.animate();
         this.ball.print();
     }
-    
+
     refreshBonus()
     {
         if (this.bonus_one.alive == true && (this.scores[0] >= 2 || this.scores[1] >= 2))
@@ -277,7 +259,7 @@ class LocalGame1v2
             this.bonus_one.animate();
             this.bonus_one.print();
         }
-    
+
         if (this.bonus_two.alive == true && (this.scores[0] >= 4 || this.scores[1] >= 4))
         {
             this.bonus_two.print();
@@ -285,18 +267,17 @@ class LocalGame1v2
             this.bonus_two.print();
         }
     }
-    
+
     resetGame()
     {
         this.scores[0] = 0;
         this.scores[1] = 0;
-    
+
         this.left_player.reset();
-        this.right_player_1.reset();
-        this.right_player_2.reset();
+        this.right_player.reset();
 
         this.alert = 0;
-    
+
         if (gameMode != "normal")
             this.bonus_one.reset(), this.bonus_two.reset();
     }
@@ -307,30 +288,40 @@ class LocalGame1v2
             this.scores[0]++;
         else
             this.scores[1]++;
-    
+
         this.ball.replace();
     }
-    
+
     isOver()
     {
         if (active == false)
             return (true);
         if (this.scores[0] > 9 || this.scores[1] > 9)
         {
-            if (this.scores[0] > 9)
+            if (type == 'tournament')
             {
-                let left_side_won = document.getElementById('left_side_won_text');
-                left_side_won.style.display = "block";
+                if (this.scores[0] > 9)
+                    t_LeftWin();
+                if (this.scores[1] > 9)
+                    t_RightWin();
             }
-            if (this.scores[1] > 9)
+            else
             {
-                let right_side_won = document.getElementById('right_side_won_text');
-                right_side_won.style.display = "block";
+                if (this.scores[0] > 9)
+                {
+                    let player_left_won = document.getElementById('left_player_won_text');
+                    player_left_won.style.display = "block";
+                }
+                if (this.scores[1] > 9)
+                {
+                    let player_right_won = document.getElementById('right_player_won_text');
+                    player_right_won.style.display = "block";
+                }
             }
-                
+
             this.resetGame();
             active = false;
-        
+
             return (true);
         }
         return (false);
@@ -339,10 +330,10 @@ class LocalGame1v2
 
 // < Initialisation > //
 
-function initializeLocal1v2()
+function initializeOnline1v1()
 {
-    players_nb = 3;
-    game = new LocalGame1v2();
+    players_nb = 2;
+    game = new OnlineGame1v1();
 
     game.initialize();
     game.refreshBackground();
@@ -351,48 +342,48 @@ function initializeLocal1v2()
 
 // < Menu display management > //
 
-function displayLocal1v2()
+function displayOnline1v1()
 {
-    let start_btn = document.getElementById('start_2v1_local');
+    let start_btn = document.getElementById('start_1v1_local');
     start_btn.style.visibility = "hidden";
-    let player_side_won = document.getElementById('left_side_won_text');
-    player_side_won.style.display = "none";
-    let right_side_won = document.getElementById('right_side_won_text');
-    right_side_won.style.display = "none";
+    let player_left_won = document.getElementById('left_player_won_text');
+    player_left_won.style.display = "none";
+    let player_right_won = document.getElementById('right_player_won_text');
+    player_right_won.style.display = "none";
 
-    let timer = document.getElementById('2v1_local_timer');
+    let timer = document.getElementById('1v1_local_timer');
     timer.style.display = "block";
 
     displayCountDown(3);
 }
 
-function removeLocal1v2()
+function removeOnline1v1()
 {
     let menu_music = document.getElementById('mgs');
     let game_music = gameMusicSelector();
 
     game_music.pause();
     menu_music.play();
-    let timer = document.getElementById('2v1_local_timer');
+    let timer = document.getElementById('1v1_local_timer');
     timer.style.display = "none";
 
-    let start_btn = document.getElementById('start_2v1_local');
+    let start_btn = document.getElementById('start_1v1_local');
     start_btn.innerHTML = getTranslation("Launch a game");
     start_btn.style.visibility = "visible";
 }
 
-function startLocal1v2()
+function startLocal1v1()
 {
     if (game.isOver() == true || active == false)
     {
         game.refreshBackground();
         game.resetGame();
         
-        removeLocal1v2();
+        removeOnline1v1();
     }
     else
     {
         game.refreshDisplay();
-        requestAnimationFrame(startLocal1v2);
+        requestAnimationFrame(startLocal1v1);
     }
 }
