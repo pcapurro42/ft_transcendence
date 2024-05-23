53a3fc61b827121cc265f06f6313b9929914596f
pcapurro42 <pcapurro@student.42nice.fr>
2024-05-23 19:19:49 +0200

    more init for online 1v1

--- host/front-end/JavaScript/Game/oneVsOneOnline.js
+++ host/front-end/JavaScript/Game/oneVsOneOnline.js
@@ -52,10 +52,10 @@ class OnlineGame1v1
     {
         // canvas creation
 
-        if (type == "tournament")
-            this.canvas = document.getElementById('tournament_game');
+        if (role == "host")
+            this.canvas = document.getElementById('one_vs_one_host_game');
         else
-            this.canvas = document.getElementById('one_vs_one_local_game');
+            this.canvas = document.getElementById('one_vs_one_guest_game');
         this.display = this.canvas.getContext('2d');
 
         this.canvas.width = this.game_width;
@@ -338,20 +338,32 @@ function initializeOnline1v1()
     game.initialize();
     game.refreshBackground();
     active = true;
+
+    // if (role == "host")
+    // {
+    //     data_channel.send("");
+    // }
 }
 
 // < Menu display management > //
 
 function displayOnline1v1()
 {
-    let start_btn = document.getElementById('start_1v1_local');
-    start_btn.style.visibility = "hidden";
-
     let timer = document.getElementById('1v1_online_timer');
+    timer.classList.remove('d-none');
     timer.style.display = "block";
 
     if (role == "host")
+    {
+        let start_btn = document.getElementById('start_1v1_online');
+        start_btn.style.visibility = "hidden";
         data_channel.send("go");
+    }
+    else
+    {
+        let waiting_msg = document.getElementById('waiting_host');
+        waiting_msg.style.visibility = "hidden";
+    }
 
     displayCountDown(3);
 }
@@ -383,6 +395,6 @@ function startOnline1v1()
     else
     {
         game.refreshDisplay();
-        requestAnimationFrame(startLocal1v1);
+        requestAnimationFrame(startOnline1v1);
     }
 }
