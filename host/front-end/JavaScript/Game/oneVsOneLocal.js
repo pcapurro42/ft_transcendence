be8a9cbd097cb875b0959296a18297d224b4d3f1
pcapurro42 <pcapurro@student.42nice.fr>
2024-06-18 18:22:40 +0200

    bonus count is now correctly registered for both 1v1 and 1v2 local situations

--- host/front-end/JavaScript/Game/oneVsOneLocal.js
+++ host/front-end/JavaScript/Game/oneVsOneLocal.js
@@ -338,8 +338,8 @@ class LocalGame1v1
             localStorage.setItem('lcl_ball_exit_nb', (parseInt(localStorage.getItem('lcl_ball_exit_nb')) + game.ball.lcl_exit).toString());
             localStorage.setItem('lcl_ball_bounce_nb', (parseInt(localStorage.getItem('lcl_ball_bounce_nb')) + game.ball.lcl_bounce).toString());
             
-            localStorage.setItem('lcl_bonus_taken_nb', (parseInt(localStorage.getItem('lcl_bonus_taken_nb')) + game.bonus_one).toString());
-            localStorage.setItem('lcl_bonus_taken_nb', (parseInt(localStorage.getItem('lcl_bonus_taken_nb')) + game.bonus_two).toString());
+            localStorage.setItem('lcl_bonus_taken_nb', (parseInt(localStorage.getItem('lcl_bonus_taken_nb')) + game.bonus_one.bonus_taken).toString());
+            localStorage.setItem('lcl_bonus_taken_nb', (parseInt(localStorage.getItem('lcl_bonus_taken_nb')) + game.bonus_two.bonus_taken).toString());
 
             return (true);
         }
