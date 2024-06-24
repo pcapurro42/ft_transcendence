344a1e5cb0867ca85e815032e5627505a5be8edb
pcapurro42 <pcapurro@student.42nice.fr>
2024-06-24 13:21:04 +0200

    adjusted wrong pathnames for IP replacement in init.sh, host container

--- host/init.sh
+++ host/init.sh
@@ -3,10 +3,10 @@
 sed -i "s|hostname|$HOSTNAME|g" /etc/nginx/conf.d/nginx.conf
 
 sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Menu/auth.js
-sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/browser_nav.js
-sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/rgpd.js
+sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Menu/browser_nav.js
+sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Menu/rgpd.js
 sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Online/answer.js
-sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/database.js.js
+sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Online/database.js
 sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Online/offer.js
 
 nginx -g 'daemon off;'
