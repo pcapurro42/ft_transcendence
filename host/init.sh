81cc844a1026758d066a22eae5760de4dd242e04
pcapurro42 <pcapurro@student.42nice.fr>
2024-06-24 13:07:17 +0200

    added IP replacement in new JavaScript files, front-end, in init.sh of host container

--- host/init.sh
+++ host/init.sh
@@ -3,8 +3,10 @@
 sed -i "s|hostname|$HOSTNAME|g" /etc/nginx/conf.d/nginx.conf
 
 sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Menu/auth.js
+sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/browser_nav.js
+sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/rgpd.js
 sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Online/answer.js
+sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/database.js.js
 sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/Online/offer.js
-sed -i "s|hostname|$HOSTNAME|g" /home/front-end/JavaScript/init.js
 
 nginx -g 'daemon off;'
